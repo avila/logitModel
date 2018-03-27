876b30a813013bb903c1f16207c09ea9982360a1
Marcelo Avila <m.rainho.avila@gmail.com>
2018-03-27 00:22:15 -0300

    weiter it goes

--- R/logitModell.R
+++ R/logitModell.R
@@ -1,102 +1,112 @@
-#' @title Maximum Likelihood
-#' @description This function calculates the maximum likelihood for binary logistic regression
-#' @param y a matrix/vector containing the dependent variable
-#' @param X a matrix containing the intercept and all independent variables
-#' @return a list with maximum likelihood estimation results
+#' Maximum Likelihood Estimation
+#'
+#' This function calculates the maximum likelihood for binary logistic regression
+#' based on the Newton Raphson Method.
+#'
+#' This function is the calculation behind the \code{\link{logitMod}}
+#' formula interface to the user.
+#'
+#' @param y Matrix/vector containing the response variable
+#' @param X Matrix/vector containing the design matrix (including intercept)
+#' @param precision Degree of precision of algorithm
+#' @param iterMax Maximum number of iterations of optimisation algorithm
+#'
+#' @return A list with maximum likelihood estimation results.
+#'
+#' @references Agresti, A (2013) Categorical Data Analysis,
+#' John Wiley & Sons, 2013, Volume 792 of Wiley Series in Probability
+#' and Statistics, ISSN 1940-6517
+#'
+#' @references Czepiel, S.A. (2002) Maximum Likelihood Estimation of Logistic
+#' Regression Models: Theory and Implementation. Available at
+#' \href{https://czep.net/stat/mlelr.pdf}{czep.net/stat/mlelr.pdf}. [visited:
+#' 25.03.2018]
+#'
 #' @examples
-#' testData <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")[1:100,]
-#' testData$rank <- factor(testData$rank)
-#' testModell <- as.formula("admit ~ gre + gpa + rank")
-#' testModelFrame <- model.frame(admit ~ gre + gpa + rank, testData)
-#' MLE(X = model.matrix(testModell, testModelFrame), y = model.response(testModelFrame))
-#' @export
-MLE <- function(y, X, tolerance = 1e-14, epsilon = 1e-10, iterMax = 1000) {
+#' set.seed(42)
+#' X1 <- rnorm(100);    X2 <- rnorm(100);
+#' points <- 2 * X1 - 3 * X2
+#' y <- rbinom(n, 1, exp(points) / (1 + exp(points)))
+#' X <- cbind(b1 = 1, b2 = X1, b3 = X2)
+#' fit <- MLE(y = y, X = X)
+MLE <- function(y, X, precision = 1e-14, iterMax = 100) {
 
-  #  Newton Raphson Method
+  # Newton Raphson Method
+  # follows Czepiel (2002) notation (page 8)
   beta <- rep(0, times = ncol(X))
-  i <- 0
+  i <- 1
   convergence <- FALSE
-  while (i <= iterMax & convergence == FALSE) { # epsilon > tolerance &
+  while (i <= iterMax & convergence == FALSE) { # epsilon > precision &
 
-    # p := probabilities
+    # calculate probabilities (mu)
     eta <- X %*% beta
-    p <- as.numeric(logist(X %*% beta))
+    mu <- as.numeric(logist(X %*% beta))
 
-    # aktualisiere ...
-    M <- diag(p * (1 - p))
+    # init / update ... Matrix W
+    W <- diag(mu * (1 - mu))
 
     # calculate and update beta
-    deltaBeta <- solve(t(X) %*% M %*% X) %*% t(X) %*% (y - p)
+    deltaBeta <- solve(t(X) %*% W %*% X) %*% t(X) %*% (y - mu)
     beta <- beta + deltaBeta
 
-    # update epsilon and i
-    epsilon <- sum(abs(deltaBeta))
-    i <- i + 1
-
-    # check non-convergence condition
-    if (epsilon < tolerance) {
+    # check convergence condition
+    if (sum(abs(deltaBeta)) < precision) {
       convergence <- TRUE
       break
     }
+
+    # update i
+    i <- i + 1
   }
+
   if (convergence == FALSE) {
-    stop(paste(i, "iterations reached: Lack of convergence"))
+    stop(paste(i, "iterations reached without convergence"))
   }
-  # DF := Nr. Observations - Nr. Parameters
+
+  # Degrees of Freedom #TODO: understand this
   dfRes <- nrow(X) - ncol(X)  # DF Residual Modell
   dfNull <- nrow(X) - 1       # DF Null Modell
 
-  # Kovarianzmatrix
-  vcov <- solve(t(X) %*% M %*% X)
+  # Compute covariace matrix (eq 22 from Czepiel, 2002)
+  vcov <- solve(t(X) %*% W %*% X)
 
-  # Devianz Residual
-  s <- y
-  s[s == 0] = -1
-  devianceResidual = as.numeric(s * sqrt(-2*((y * eta) - (log(1 + exp(eta))))))
+  # Compute residual deviance TODO: learn this thing here
+  s <- ifelse(y == 0, -1, y)
+  devianceResidual <- as.numeric(s * sqrt(-2*((y * eta) - (log(1 + exp(eta))))))
 
-  #https://onlinecourses.science.psu.edu/stat504/node/62
-
-  # Maximumwert der Log Likelihood Funktion
-  maxLogLikeValue <- (sum((y * X %*% beta) - (log(1 + exp(X %*% beta)))))
-
-  # Fitted Values
-  fittedWerte <- p
-  M <- M # TODO: whats is this?
+  # calculate max value of logLikelihood
+  maxLL <- (sum((y * X %*% beta) - (log(1 + exp(X %*% beta)))))
 
   # Liste der zurückgegebenen Werte
   result <- list(coefficients = beta,
+                 ## residuals??
+                 fittedValues = mu,
                  vcov = vcov,
                  devianceResidual = devianceResidual,
                  dfRes = dfRes,
                  dfNull = dfNull,
-                 maxLogLikeValue = maxLogLikeValue,
-                 fittedWerte = fittedWerte,
-                 M = M,
+                 maxLL = maxLL,
+                 W = W,
 
                  iterCount = i)
   return(result)
-
 }
 
-## Helper Functions
-logist <- function(eta) return(exp(eta) / (1 + exp(eta)))
-
-
-#' @title Interface for an alternative logistic regression implementation
-#' @description This function computes coefficients of a binary logistic regression
-#' and contructs an object of "logitMod" class
-#' @param formula a formula object. Matrix or array are not accepted
-#' @param data a data frame contains all variables
-#' @return a list of class "logitMod" containing the coefficients, a null model, formula, call,
-#' the dependent variable and all independent variables.
+#' Alternative Interface for Logistic Regression Modelling
+#'
+#' \code{logitMod} computes a logistic regression for a binary response variable
+#'
+#' @param formula
+#' @param data
+#' @param formula
+#' @param formula
+#'
+#' @return
+#'
 #' @examples
-#' testData <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")[1:100,]
-#' testData$rank <- factor(testData$rank)
-#' testModell <- as.formula("admit ~ gre + gpa + rank")
-#' testModelFrame <- model.frame(admit ~ gre + gpa + rank, testData)
-#' logitMod(formula = admit ~ gre + gpa + rank, data = testData)
+#'
 #' @export
-logitMod <- function(formula, data) {
+logitMod <- function(formula, data, precision = 1e-14, iterMax = 100) {
 
   # generate model.frame, extract design matrix (X) and response var (y)
   modelFrame <- model.frame(formula, data)
@@ -104,36 +114,35 @@ logitMod <- function(formula, data) {
   y <- model.response(modelFrame)
 
   # TODO: sanity checks
-
-  # falls y nicht als 0-1/Variable eingegeben wird
-  if (!(0 %in% y && 1 %in% y)) {
-    y <- factor(y, labels = c(0,1))
+  if (FALSE) {
+    TRUE
   }
-  y <- as.numeric(as.character(y))
 
   # conduct ML-Estimation for Full Model
-  result <- MLE(y, X)
+  result <- MLE(y, X, precision = precision, iterMax = iterMax)
 
   # conduct ML-Estimation for Null Model
-  nullModell <- MLE(y = y, X = matrix(rep(1, times = nrow(X)), ncol = 1))
+  nullModell <- MLE(y = y, X = matrix(rep(1, times = nrow(X)),ncol = 1),
+                    precision = precision, iterMax = iterMax)
   result$nullModell <- nullModell
 
-  # speichere notwendige Parameter in die Ergebnisliste
+  # Results
   result$formula <- formula
   result$call <- match.call()
   result$X <- X
   result$y <- y
 
-  # ordne die Ergebnisliste der Klasse "logitMod" zu
+  # Assign Class
   class(result) <- "logitMod"
 
-  return(result)
-
+  result
 }
 
 
-#' @title Printing method for logitMod estimations
-#' @description Printing method for class "logitMod"
+#' Printing method for logitMod estimations
+#'
+#' Printing method for class "logitMod"
+#'
 #' @param x an object of class "logitMod"
 #' @param ... unused parameter, methods are required to have same
 #' arguments as their generic functions
@@ -158,11 +167,12 @@ print.logitMod <- function(x, ...){
       x$dfRes, " Residual")
 
   # Berechnung von null deviance, residual deviance & aic
+  # TODO: take it out of here
   nullDeviance <- sum(x$nullModell$devianceResidual^2)
   x$nullDeviance <- nullDeviance
   devianceResidual <- sum(x$devianceResidual^2)
   x$devianceResidual <- devianceResidual
-  x_AIC <- (-2*x$maxLogLikeValue + 2*ncol(x$X))
+  x_AIC <- (-2*x$maxLL + 2*ncol(x$X))
   x$AIC <- x_AIC
 
   cat("\nNull Deviance:\t", round(nullDeviance,1))
@@ -175,8 +185,10 @@ print.logitMod <- function(x, ...){
 }
 
 
-#' @title Summary method for "logitMod" estimations
-#' @description Summary method for class "logitMod"
+#' Summary method for "logitMod" estimations
+#'
+#' Summary method for class "logitMod"
+#'
 #' @param object an object of class "logitMod"
 #' @param ... unused parameter, methods are required to have same
 #' arguments as their generic functions
@@ -215,7 +227,7 @@ summary.logitMod <- function(object, ...) {
   object$nullDeviance <- nullDeviance
   residualDeviance <- sum(object$devianceResidual^2)
   object$residualDeviance <- residualDeviance
-  x_AIC <- (-2*object$maxLogLikeValue + 2*ncol(object$X))
+  x_AIC <- (-2*object$maxLL + 2*ncol(object$X))
   object$AIC <- x_AIC
 
   class(object) <- "summary.logitMod"
@@ -225,8 +237,10 @@ summary.logitMod <- function(object, ...) {
 }
 
 
-#' @title Printing method for the summary of "logitMod" estimations
-#' @description Printing method for summary of class "logitMod"
+#' Printing method for the summary of "logitMod" estimations
+#'
+#' Printing method for summary of class "logitMod"
+#'
 #' @param x an object of class "logitMod"
 #' @param ... unused parameter, methods are required to have same
 #' arguments as their generic functions
@@ -276,32 +290,34 @@ print.summary.logitMod <- function(x, ...) {
 #' logm <- logitMod(formula = admit ~ gre + gpa + rank, data = testData)
 #' plot(logm)
 #' @export
-plot.logitMod <- function(x, ...) {
+plot.logitMod <- function(x, which = 1, ...) {
 
-  #1
-  plot(y = x$devianceResidual, x = (x$X %*% x$coefficients),
-       main = "Residuals vs Fitted",
-       ylab = "Residuals",
-       xlab = paste("Predicted Values\n",
-                    deparse(x$call)))
-  abline(a = 0, b = 0, lty = 3)
-
-  #2
-  qqnorm(x$devianceResidual,
-         main = "Normal Q-Q",
-         ylab = "Std. deviance resid.",
-         xlab = paste("Theoretical Quantiles\n", deparse(x$call)))
-  qqline(x$devianceResidual, lty = 3)
-
-  #3
-  plot(y = sqrt(abs(x$devianceResidual)), x = (x$X %*% x$coefficients),
-       main = "Scale Location",
-       ylab = expression(sqrt("|Std. deviance resid.|")),
-       xlab = paste("Predicted Values\n", deparse(x$call)))
+  if (which == 1) {
+    plot(y = x$devianceResidual, x = (x$X %*% x$coefficients),
+         main = "Residuals vs Fitted",
+         ylab = "Residuals",
+         xlab = paste("Predicted Values\n",
+                      deparse(x$call)))
+    abline(a = 0, b = 0, lty = 3)
+  } else if (which == 2) {
+    qqnorm(x$devianceResidual,
+           main = "Normal Q-Q",
+           ylab = "Std. deviance resid.",
+           xlab = paste("Theoretical Quantiles\n", deparse(x$call)))
+    qqline(x$devianceResidual, lty = 3)
+  } else {
+    plot(y = sqrt(abs(x$devianceResidual)), x = (x$X %*% x$coefficients),
+         main = "Scale Location",
+         ylab = expression(sqrt("|Std. deviance resid.|")),
+         xlab = paste("Predicted Values\n", deparse(x$call)))
+  }
 
   # #4 shittt
   # pearsonResidual <-
-  #     (x$y - x$fittedWerte)/sqrt(x$fittedWerte*(1 - x$fittedWerte))
-  # plot(y = pearsonResidual, x = diag(x$M))
-
+  #     (x$y - x$fittedValues)/sqrt(x$fittedValues*(1 - x$fittedValues))
+  # plot(y = pearsonResidual, x = diag(x$W))
 }
+
+
+## Helper Functions
+logist <- function(eta) return(exp(eta) / (1 + exp(eta)))
--- tests/testthat/test-logitModel.R
+++ tests/testthat/test-logitModel.R
@@ -1,9 +1,22 @@
-# testData <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")[1:100,]
-# testData$rank <- factor(testData$rank)
-# testModell <- as.formula("admit ~ gre + gpa + rank")
-# testModelFrame <- model.frame(admit ~ gre + gpa + rank, testData)
+####------------
+testData <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")[1:100,]
+testData$rank <- factor(testData$rank)
+####------------
+
+# debugonce(MLE)
 myFit <- logitMod(formula = admit ~ gre + gpa + rank, data = testData)
 statFit <- glm(formula = admit ~ gre + gpa + rank, data = testData, family = binomial)
 
 myFit
 statFit
+## -----------------------------------
+rm(myFit, statFit)
+##------------------------------------
+set.seed(42)
+X1 <- rnorm(100); X2 <- rnorm(100)
+points <- 2 * X1 - 3 * X2
+y <- rbinom(n, 1, exp(points) / (1 + exp(points)))
+X <- cbind(b1 = 1, b2 = X1, b3 = X2)
+fit <- MLE(y = y, X = X)
+fit$coefficients
+
